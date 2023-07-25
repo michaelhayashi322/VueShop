a969b3e609563ffde4ee163f468af15cf4f39ba0
Michael Hayashi <michaelhayashi322@gmail.com>
2023-07-25 20:50:32 +0900

    main functionalities of CRUD

--- src/views/CrudView.vue
+++ src/views/CrudView.vue
@@ -2,34 +2,73 @@
 export default {
   data(){
     return {
-      
+      all_names: [
+        {First: "Emil", Last: "Hans"},
+        {First: "Mustermann", Last: "Max"},
+        {First: "Tisch", Last: "Roman"},
+      ],
+      selected_name: '',
+      current_first: '',
+      current_last: '',
+      search_key: '',
     }
   },
   computed: {
-
+    filteredNames() {
+      return this.all_names.filter((name) => name.First.indexOf(this.search_key)>-1 || name.Last.indexOf(this.search_key) >-1)
+    }
   },
   watch: {
-
+    selected_name(newSelected){
+      this.current_first = this.all_names.filter((name) => name.First + " " + name.Last == newSelected)[0].First;
+      this.current_last = this.all_names.filter((name) => name.First + " " + name.Last == newSelected)[0].Last;
+    }
   },
   methods: {
     create(){
-
+      this.all_names.push({First: this.current_first, Last: this.current_last});
     },
     update(){
-
+      this.all_names.map((name, index)=>{
+        if(name.First + " " + name.Last == this.selected_name){
+          this.all_names[index].First = this.current_first
+          this.all_names[index].Last = this.current_last
+          this.selected_name = this.current_first + " " + this.current_last
+          return
+        }
+      })
     },
-    delete(){
-
+    del(){
+      this.all_names = this.all_names.filter((name) => name.First + " " + name.Last != this.selected_name)
+      this.current_first = ''
+      this.current_last = ''
     }
-  }
+  },
 }
 </script>
 
 <template>
   <div class="about">
     <h1>This is an crud page</h1>
-    <label>Name: <input v-model="first"></label>
-  <label>Surname: <input v-model="last"></label>
+    <div class="searchs">
+      <input v-model="search_key" placeholder="search names here">
+    </div>
+    <div class="selects">
+      <select size="10" v-model="selected_name">
+        <option v-for="name in filteredNames" :key="name.id">
+          {{ name.First + " " + name.Last}}
+        </option>
+      </select>
+    </div>
+    <div class="inputs">
+      <input v-model="current_first" placeholder="First Name">
+      <input v-model="current_last" placeholder="Last Name">
+    </div>
+    <div class="buttons">
+      <button @click="create">Create</button>
+      <button @click="update">Update</button>
+      <button @click="del">Delete</button>
+    </div>
   </div>
 </template>
 
@@ -40,5 +79,27 @@ export default {
     display: flex;
     align-items: center;
   }
+}
+* {
+  font-size: inherit;
+}
+
+input {
+  display: block;
+  margin-bottom: 10px;
+}
+
+select {
+  float: left;
+  margin: 0 1em 1em 0;
+  width: 14em;
+}
+
+.buttons {
+  clear: both;
+}
+
+button + button {
+  margin-left: 5px;
 }
 </style>
